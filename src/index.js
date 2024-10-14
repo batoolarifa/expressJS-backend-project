f1c20b385cf6d51c9c3c4d7c439133ba41f3944c
batoolarifa <batoolarifa73@gmail.com>
2024-10-15 02:50:01 +0500

    Add utility function for ApiError and Api Response and asyncHandler

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 
+
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then(() => {
+    app.on("error",(error) => {
+        console.log("ERROR: " , error);
+        
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!," , err);
+})
 
 
 
